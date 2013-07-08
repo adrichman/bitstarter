aa2d13cbe68b0ca7a73851c3ac006cfca3f529c4
Adam D.Richman <ar@adamrichman.com>
2013-07-08 23:00:17 +0000

    fixed semi-colon line 11

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var bufIndex = new Buffer(readIndex, "utf-8");
 
 app.get('/', function(request, response) {
     response.send(bufIndex.toString());
-};
+});
  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
