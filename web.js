d4481823549be5d0890821ea3029e4d4804e5e4d
Adam D.Richman <ar@adamrichman.com>
2013-07-08 22:44:56 +0000

    added semi colon to debug web.js

--- web.js
+++ web.js
@@ -7,14 +7,15 @@ var readIndex = new fs.readFileSync('~/index.html');
 var bufString = function() { 
     new Buffer(readIndex, "utf-8");
     self.toString();
-}
+};!
 
 app.get('/', function(request, response) {
     response.send(bufString);
-
+};
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-  
+   
 
