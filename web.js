d605eef0572820355923c92cb0601f36a5346405
Adam D.Richman <ar@adamrichman.com>
2013-07-08 22:32:30 +0000

    Added web.js, wrote functionto place index.html content into the buffer, buffer to string. Adapted function to return the string to the browser.

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var readIndex = new fs.readFileSync('~/index.html');
+
+var bufString = function() { 
+    new Buffer(readIndex, "utf-8");
+    self.toString();
+}
+
 app.get('/', function(request, response) {
-    response.send('Hello World 2!');
-});
+    response.send(bufString);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+  
+
