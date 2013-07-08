6e8830875a3de94a4be5b3c07d7b9c7cee2bbd12
Adam D.Richman <ar@adamrichman.com>
2013-07-08 22:55:01 +0000

    instantiated bufIndex to call .ToString on it

--- web.js
+++ web.js
@@ -2,15 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var readIndex = new fs.readFileSync('~/index.html');
+var readIndex = new fs.readFileSync('index.html');
 
-var bufString = function() { 
-    new Buffer(readIndex, "utf-8");
-    self.toString();
-};!
+var bufIndex = new Buffer(readIndex, "utf-8");
 
 app.get('/', function(request, response) {
-    response.send(bufString);
+    response.send(bufIndex.toString());
 };
  
 var port = process.env.PORT || 5000;
