0f42add4f61f89417d0735082d302dedb3475779
Ubuntu <ubuntu@ip-172-31-18-240.us-west-2.compute.internal>
2013-07-22 21:30:28 +0000

    removed /public specification for serving local files

--- web.js
+++ web.js
@@ -6,11 +6,13 @@ var readIndex = new fs.readFileSync('index.html');
 
 var bufIndex = new Buffer(readIndex, "utf-8");
 
+app.use(express.static(__dirname));
+
 app.get('/', function(request, response) {
     response.send(bufIndex.toString());
 });
  
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
